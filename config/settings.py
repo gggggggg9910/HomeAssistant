d3d752099f02600c9f706e7bb5a696ff9d41db32
gggggggggg9910 <wudixingg@qq.com>
2025-12-07 14:37:39 +0800

    提交

--- config/settings.py
+++ config/settings.py
@@ -80,6 +80,7 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
         env_nested_delimiter = "__"
+        extra = "allow"
 
 
 # Global settings instance
