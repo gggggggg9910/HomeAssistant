af88e207ecc01665f5b940b61cbe543e0c471c19
gggggggggg9910 <wudixingg@qq.com>
2025-12-13 13:20:34 +0800

    fix

--- download_model.py
+++ download_model.py
@@ -13,7 +13,7 @@ def main():
         print("Testing imports...")
         import sys
         sys.path.append('/home/wudixin/models/CosyVoice/third_party/Matcha-TTS')
-        from cosyvoice.cli.cosyvoice import CosyVoice, CosyVoice2
+        from cosyvoice.cli.cosyvoice import CosyVoice2
         from cosyvoice.utils.file_utils import load_wav
         import torchaudio
 
